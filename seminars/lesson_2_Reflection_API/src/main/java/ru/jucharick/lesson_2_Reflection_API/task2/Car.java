3f2d2c05d2e17dd373585e242f575aedd73e7a11
Julia Ch <juliecha@yandex.ru>
2023-12-03 15:13:07 +0300

    Add seminar - lesson_2_Reflection_API - task2

---
+++ seminars/lesson_2_Reflection_API/src/main/java/ru/jucharick/lesson_2_Reflection_API/task2/Car.java
@@ -0,0 +1,16 @@
+package ru.jucharick.lesson_2_Reflection_API.task2;
+
+public class Car {
+
+    private String model;
+
+    private String color;
+
+    private int year;
+
+    public Car(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+}
